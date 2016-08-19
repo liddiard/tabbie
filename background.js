c41af9acf8498d994b2cc695cdf06c4c458a618b
Harrison Liddiard <harrison.liddiard@nytimes.com>
2016-08-19 18:08:08 -0400

    second commit

--- background.js
+++ background.js
@@ -1,10 +1,35 @@
-console.log('background js loaded');
+let idToFaviconMap = {};
+let numbersDisplayed = false;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  chrome.tabs.query({}, tabs => {
-    tabs.forEach(tab => {
-      console.log(tab.favIconUrl);
-    });
-  });
-  return true;
+  switch (request) {
+    case 'showNumbers':
+      if (!numbersDisplayed) {
+        numbersDisplayed = true;
+        idToFaviconMap = {};
+        chrome.tabs.query({}, tabs => {
+          let tabId;
+          tabs.forEach(tab => {
+            idToFaviconMap[tab.id] = tab.favIconUrl;
+            if (tab.index < 8) {
+              chrome.tabs.sendMessage(tab.id, {favIconUrl: chrome.extension.getURL(`tab-icons/${tab.index+1}.png`)});
+            } else if (tab.index === tabs.length - 1) {
+              chrome.tabs.sendMessage(tab.id, {favIconUrl: chrome.extension.getURL(`tab-icons/9.png`)});
+            }
+          });
+        });
+      }
+      return true;
+    case 'showFavIcons':
+      numbersDisplayed = false;
+      chrome.tabs.query({}, tabs => {
+        tabs.forEach(tab => {
+          chrome.tabs.sendMessage(tab.id, {favIconUrl: idToFaviconMap[tab.id]});
+        });
+      });
+      return true;
+    default:
+      console.error('Unrecognized request: ' + request); 
+      return false;
+  }
 });
--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,46 @@
-document.addEventListener('keydown', event => {
-  console.log('keydown', event.keyCode);
-  chrome.runtime.sendMessage({event: 'keypress'}, response => {
+let metaKeyDown = false;
 
-  });
+document.addEventListener('keydown', event => {
+  if (event.metaKey && !metaKeyDown) {
+    metaKeyDown = true;
+    chrome.runtime.sendMessage('showNumbers');
+  }
+  else if (metaKeyDown) {
+    metaKeyDown = false; 
+    chrome.runtime.sendMessage('showFavIcons');
+  }
 }, false);
+
+document.addEventListener('keyup', event => {
+  // console.log('keyUp', event.keyCode);
+  if (metaKeyDown || event.keyCode === 91 || event.keyCode === 17) {
+    metaKeyDown = false;
+    chrome.runtime.sendMessage('showFavIcons');
+  }
+}, false);
+
+document.addEventListener('visibilitychange', event => {
+  console.log('visibility change');
+  if (metaKeyDown) {
+    metaKeyDown = false;
+    chrome.runtime.sendMessage('showFavIcons');
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  el = document.querySelectorAll('head link[rel*="icon"]');
+
+  // Remove existing favicons
+  Array.prototype.forEach.call(el, node => {
+    node.parentNode.removeChild(node);
+  });      
+  // Create new favicon
+  link      = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel  = 'icon';
+  link.href = request.favIconUrl;
+
+  document.getElementsByTagName('head')[0].appendChild(link);
+
+  return true;
+});
